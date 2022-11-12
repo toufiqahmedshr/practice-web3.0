4bc84a31817c2be78746ebd188d5a72d641259e1
Toufiq Shishir <toufiq_a24@outlook.com>
2022-11-12 10:26:15 +0600

    a

--- simple-storage/StorageFactory.sol
+++ simple-storage/StorageFactory.sol
@@ -4,9 +4,20 @@ pragma solidity ^0.8.7;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    // SimpleStorage public simpleStorage;
 
-    function createSimpleStorage() public {
-        simpleStorage = new SimpleStorage();
+    // function createSimpleStorage() public {
+    //     simpleStorage = new SimpleStorage();
+    // }
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorage() public returns(SimpleStorage){
+        SimpleStorage simpleStorage = new SimpleStorage();
+        return simpleStorage;
+    }
+
+    function addSimpleStorage() public {
+        simpleStorageArray.push(createSimpleStorage());
     }
 }
